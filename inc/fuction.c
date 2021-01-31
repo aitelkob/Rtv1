d05894985942f2e38f9967029fa53e129587ddf8
aitelkob <yait-el-@student.1337.ma>
2021-01-31 15:12:54 +0100

    Vector calc  done

--- inc/fuction.c
+++ inc/fuction.c
@@ -6,7 +6,7 @@
 /*   By: yait-el- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/29 17:22:35 by yait-el-          #+#    #+#             */
-/*   Updated: 2021/01/29 17:32:58 by yait-el-         ###   ########.fr       */
+/*   Updated: 2021/01/31 15:06:04 by yait-el-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,18 @@
 
 #include "Rtv1.h"
 
+//////////////// parsing
 void        parce(char *av,t_rtv *rtv);
 
+
+
+////////////////// calc_vect.c
+t_vector add(t_vector vec1, t_vector vec2);
+t_vector sub(t_vector vec1, t_vector vec2);
+t_vector multi(t_vector vec1, double t);
+t_vector Div(t_vector vec1, double t);
+double length_squared(t_vector vec1, t_vector vec2);
+double length(t_vector vec1, t_vector vec2);
+
+//////////
 #endif
